6726e6ce6d46dacc80a418ba9f80089e58ca9228
Yurim Kang <yoorim7480@gmail.com>
2023-04-13 15:41:11 -0400

    added operate function

--- myscript.js
+++ myscript.js
@@ -1,3 +1,19 @@
-let num = document.getElementById("left").addEventListener("click", function (e {
-    if 
-}))
+let first = 0;
+let second = 0;
+let op = '';
+
+function operate (no, ope, nt) {
+    if (ope === '+') {
+        return no+nt;
+    } else if (ope === '-') {
+        return no-nt;
+    } else if (ope === 'x') {
+        return no*nt;
+    } else if (ope === '/') {
+        return no/nt;
+    }
+    return ;
+}
+
+function populate ()
+
